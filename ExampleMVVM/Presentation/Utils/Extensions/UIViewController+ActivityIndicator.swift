a9fb2d0fb14a008088920db0654a1bd92d3bae1c
Luigi Mazzarella <luigi.mazzarella@deltatre.com>
2024-05-27 09:51:34 +0200

    Fixed deprecated methods and properties

--- ExampleMVVM/Presentation/Utils/Extensions/UIViewController+ActivityIndicator.swift
+++ ExampleMVVM/Presentation/Utils/Extensions/UIViewController+ActivityIndicator.swift
@@ -3,18 +3,9 @@ import UIKit
 extension UITableViewController {
 
     func makeActivityIndicator(size: CGSize) -> UIActivityIndicatorView {
-        let style: UIActivityIndicatorView.Style
-        if #available(iOS 12.0, *) {
-            if self.traitCollection.userInterfaceStyle == .dark {
-                style = .white
-            } else {
-                style = .gray
-            }
-        } else {
-            style = .gray
-        }
-
-        let activityIndicator = UIActivityIndicatorView(style: style)
+       
+        let activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator.tintColor =  self.traitCollection.userInterfaceStyle == .dark ? .white : .gray
         activityIndicator.startAnimating()
         activityIndicator.isHidden = false
         activityIndicator.frame = .init(origin: .zero, size: size)
--- ExampleMVVM/Presentation/Utils/LoadingView.swift
+++ ExampleMVVM/Presentation/Utils/LoadingView.swift
@@ -7,16 +7,20 @@ class LoadingView {
     static func show() {
         DispatchQueue.main.async {
             NotificationCenter.default.addObserver(self, selector: #selector(update), name: UIDevice.orientationDidChangeNotification, object: nil)
-            if spinner == nil, let window = UIApplication.shared.keyWindow {
+            if spinner == nil, 
+                let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) {
+                
                 let frame = UIScreen.main.bounds
                 let spinner = UIActivityIndicatorView(frame: frame)
                 spinner.backgroundColor = UIColor.black.withAlphaComponent(0.2)
-                spinner.style = .whiteLarge
+                spinner.style = .large
+                spinner.tintColor = .white
                 window.addSubview(spinner)
-
+                
                 spinner.startAnimating()
                 self.spinner = spinner
             }
+
         }
     }
 
